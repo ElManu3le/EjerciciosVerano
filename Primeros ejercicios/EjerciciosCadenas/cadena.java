b588715b1afd66e3ba6043d3cd921a771ba0c053
Manuel Sancho <msancho620@yahoo.es>
2020-07-19 20:23:08 +0200

    Version final cadena.java

--- Primeros ejercicios/EjerciciosCadenas/cadena.java
+++ Primeros ejercicios/EjerciciosCadenas/cadena.java
@@ -3,30 +3,26 @@ package EjerciciosCadenas;
 import java.util.Scanner;
 
 public class cadena {
+    /*
+     * Leer una frase de máximo 80 caracteres y escribirla progresivamente es decir
+     * si la frase es: “ciervo”, el programa debe imprimir: c ci cie cier cierv
+     * ciervo
+     */
 
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        System.out.println("Escribe una fase de maximo 80 caracteres");
         String cadena = "";
-        cadena = teclado.nextLine();
-        String letras = "";
 
-        if ((cadena.length() != 0) || (cadena.length() > 80)) {
-            for (int x = 1; x <= cadena.length(); x++) {
-                char cl = cadena.charAt(x);
-                if ((cl > 64) && (cl < 91)) {
-                    letras = letras + cl;
-                }
-                if ((cl > 96) && (cl < 122)) {
-                    letras = letras + cl;
-                }
+        do {
+            System.out.println("Escribe una fase de maximo 80 caracteres");
 
-            }
-            System.out.println("La cadena escrita tiene" + cadena.length() + "caracteres");
-            System.out.println(letras);
+            cadena = teclado.nextLine();
 
-        } else {
-            System.out.println("vuelve a escribir la frase");
+        } while (cadena.length() > 80);
+
+        for (int x = 1; x <= cadena.length(); x++) {
+
+            System.out.println(cadena.substring(0, x));
 
         }
 
