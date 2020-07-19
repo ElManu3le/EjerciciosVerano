7a1640ec5441df884f1ac6ec0a003ae2fe75051c
Ruben Sancho <17210993+xposix@users.noreply.github.com>
2020-07-19 18:26:28 +0100

    Comentarios en Javadoc

--- EjerciciosV2/EjerciciosV2/ejercicio4.java
+++ EjerciciosV2/EjerciciosV2/ejercicio4.java
@@ -4,11 +4,13 @@ import java.util.Scanner;
 
 public class ejercicio4 {
     public static void main(String[] args) {
-        // Realizar una función que calcule (muestre en pantalla) el área o el volumen
-        // de un cilindro, según se especifique.
-        // Para distinguir un caso de otro se le pasará el carácter 'a' (para área) o
-        // 'v' (para el volumen). Además hemos de pasarle a la función el radio y la
-        // altura.
+        /* 
+        Realizar una función que calcule (muestre en pantalla) el área o el volumen
+        de un cilindro, según se especifique.
+        Para distinguir un caso de otro se le pasará el carácter 'a' (para área) o
+        'v' (para el volumen). Además hemos de pasarle a la función el radio y la
+        altura. 
+        */ 
 
         int opcion, radio, altura, base;
         double pi = 3.14;
