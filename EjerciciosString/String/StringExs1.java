4934231115f1a674fafee42ece78e47ee9601a19
Manuel Sancho <msancho620@yahoo.es>
2020-07-26 17:11:36 +0200

    Más EjerciciosStrings

---
+++ EjerciciosString/String/StringExs1.java
@@ -0,0 +1,12 @@
+public class StringExs1 {
+    public static void main(String[] args) {
+        /*
+         * Escribir un programa que pida por teclado una cadena y una letra, de manera
+         * que ponga en mayúsculas cada ocurrencia de la letra que haya en la cadena y
+         * lo muestre por pantalla.
+         */ 
+
+         
+    }
+    
+}
---
+++ EjerciciosString/String/StringExs2.java
@@ -0,0 +1,7 @@
+public class StringExs2 {
+    public static void main(String[] args) {
+        
+
+    }
+    
+}
