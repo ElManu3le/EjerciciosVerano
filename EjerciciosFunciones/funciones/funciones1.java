3570481d424e3c622dd6feb928920f9f764f6b04
Manuel Sancho <msancho620@yahoo.es>
2020-07-26 15:37:46 +0200

    Enunciados

--- EjerciciosFunciones/funciones/funciones1.java
+++ EjerciciosFunciones/funciones/funciones1.java
@@ -1,6 +1,9 @@
 package Funciones;
 
 public class ejFuncion1 {
+    public static void main(String[] args) {
+        /*Devuelve verdadero si el número introducido es capicua y falso si no lo es */
+    }
     
     
 }
--- EjerciciosFunciones/funciones/funciones2.java
+++ EjerciciosFunciones/funciones/funciones2.java
@@ -2,6 +2,7 @@ package Funciones;
 
 public class funciones2 {
     public static void main(String[] args) {
+        /*Devuelve verdadero en caso de que el número introducido sea un número primo, en caso contrario devolvera un falso */
         
     }
     
