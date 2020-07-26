c1b809b51893d5399f72e0396e1fd21c364efd42
Manuel Sancho <msancho620@yahoo.es>
2020-07-26 17:12:23 +0200

    Intento de ...

--- EjerciciosFunciones/funciones/funciones1.java
+++ EjerciciosFunciones/funciones/funciones1.java
@@ -1,9 +1,26 @@
 package Funciones;
 
+import java.util.Scanner;
+
+import sun.tools.tree.EqualExpression;
+
 public class ejFuncion1 {
     public static void main(String[] args) {
-        /*Devuelve verdadero si el número introducido es capicua y falso si no lo es */
+        Scanner teclado = new Scanner(System.in);
+        /* Devuelve verdadero si el número introducido es capicua y falso si no lo es */
+
+        // int num;
+        String num = "";
+
+        System.out.println("Dame un numero para ver si es capicua");
+        // num = teclado.nextInt();
+
+        for (int i = 0; i < num.length(); i++) {
+            
+            
+
+        }
+
     }
-    
-    
+
 }
