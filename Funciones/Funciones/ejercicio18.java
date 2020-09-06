6c8af3f98839c34fe2246114fb553aee0174afc6
ElManuele Sancho M <msancho620@yahoo.es>
2020-09-06 16:47:59 +0200

    Ejercicios sin terminar

--- Funciones/Funciones/ejercicio18.java
+++ Funciones/Funciones/ejercicio18.java
@@ -6,17 +6,14 @@ public class ejercicio18 {
     public static void main(String[] args) {
         // Pasar un numero decimal a un numero binario
         Scanner teclado = new Scanner(System.in);
-        int num, coeficiente=0, division=0, resto=0;
+        int num;
 
         System.out.println("Introduce un numero decimal");
         num = teclado.nextInt();
 
         
 
-        while (coeficiente!=0) {
-           
-            
-        }
+        
 
        
       
---
+++ Funciones/Funciones/ejercicio8.java
@@ -0,0 +1,8 @@
+package Funciones;
+
+public class ejercicio8 {
+    public static void main(String[] args) {
+        //
+    }
+    
+}
